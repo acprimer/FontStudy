8b1aa56251dd732bb1f5705c796ec7b60c0d85ca
acprimer <729101421@qq.com>
2020-04-01 22:43:41 +0800

    Initial commit

---
+++ app/src/main/java/com/ttt/fontstudy/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ttt.fontstudy
+
+import android.graphics.Typeface
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val typeface = Typeface.createFromAsset(assets, "fonts/NotoSerif-Regular.ttf")
+//        tvText.typeface = typeface
+    }
+}
